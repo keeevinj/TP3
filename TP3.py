194f8ab4287f2e7632077d4a783ff52c21c71261
Fusilli25 <jp.federico25@gmail.com>
2024-09-27 07:46:20 +0930

    Carga de Estudiantes
    La carga de los estudiantes es automatica. Esto incluye la funcion de carga de fecha de nacimiento, variables con info y un for para hacer la carga automatica

--- TP3.py
+++ TP3.py
@@ -70,7 +70,88 @@ def inicializar_arr(arreglo,tamaño, clase):
 	for i in range(tamaño):
 		arreglo[i]=clase()
 
-	
+
+
+def rdob():
+
+    rdd = str(random.randint(1,28))
+    rmm = str(random.randint(1,12))
+    ryear = str(random.randint(CYEARI,CYEARF))
+    rdate = str(ryear) + "-" + str(rmm) + "-" + str(rdd)
+    return rdate
+
+
+#VARIABLES CON INFO
+currentdate = datetime.now()
+CYEAR = int(currentdate.year)
+CYEARF = CYEAR - 18
+CYEARI = CYEAR - 60
+
+
+nombre1 = "Juan Faccio"
+nombre2 = "Julian Perez"
+nombre3 = "Kevin Dorko"
+nombre4 = "Maximo Jesus Gomez"
+
+materia1 = "Fisica"
+materia2 = "Matematica"
+materia3 = "Algebra"
+materia4 = "Algoritmo"
+materia5 = "Quimica"
+materia6 = "Ingles"
+materia7 = "Logica"
+materia8 = "Sintaxis"
+
+deporte1 = "Futbol"
+deporte2 = "Escalada"
+deporte3 = "Tennis"
+deporte4 = "PingPong"
+
+hobbies1 = "Cocinar"
+hobbies2 = "Programar"
+hobbies3 = "Gamer"
+hobbies4 = "Poesia"
+
+biografia1 = "Trabajo desde los 18 anios. Me gusta la cocina y soy amante de los perros. Mi favorito es el Siberiano"
+biografia2 = "Vivo en Villa Gobernador Galvez, soy empresario exitoso con mucho dinero y tres novias. No suenies tu vida, vivi tus suenios"
+biografia3 = "Me gusta estudiar y cuando crezca quiero ser profesor como Milton"
+biografia4 = "Quiero ser Ingeniero en sistemas porque me gusta el dinero y si voy a ser explotado, prefiero ser explotado desde la comodidad de mi casa"
+
+pais = "Argentina"
+
+ciudad1 = "Rosario"
+ciudad2 = "VGG"
+ciudad3 = "Perez"
+ciudad4 = "Rufino"
+
+
+
+def cargar_archivo_estudiantes(path):
+    global archivo_logico_estudiantes
+    archivo_logico_estudiantes.seek(0, 0)
+    estudiante = estudiantes()
+    for i in range(0, 3):
+        estudiante.email = "estudiante" + str(i + 1) + "@ayed.com"
+        estudiante.nombre = globals()[f"nombre{i + 1}"]
+        estudiante.sexo = "M"
+        estudiante.contraseña = str(111222 + 111111 * i)
+        estudiante.estado = 1
+        estudiante.hobbies = globals()[f"hobbies{i + 1}"]
+        estudiante.materia_fav = globals()[f"materia{i + 1}"]
+        estudiante.deporte_fav = globals()[f"deporte{i + 1}"]
+        estudiante.materia_fuerte = globals()[f"materia{random.randint(1, 4)}"]
+        estudiante.materia_debil = globals()[f"materia{random.randint(4, 8)}"]
+        estudiante.biografia = globals()[f"biografia{i + 1}"]
+        estudiante.pais = globals()["pais"]
+        estudiante.ciudad = globals()[f"ciudad{i + 1}"]
+        estudiante.fecha_nacimiento = rdob()
+        formato(estudiante)
+        pickle.dump(estudiante, archivo_logico_estudiantes)
+        archivo_logico_estudiantes.flush()
+
+    archivo_logico_estudiantes.close()
+
+
 
 def cargar_archivo_administradores(path):
 	global archivo_logico_administradores
