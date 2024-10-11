5e39fe4872a20fdd715078ac34e5d6c8bb72840f
Fusilli25 <jp.federico25@gmail.com>
2024-10-11 13:10:33 +1030

    Update administrador.py

--- administrador.py
+++ administrador.py
@@ -10,10 +10,10 @@ def totaldereportesignorados()
 	tam=os.path.getsize(archivo_fisico_reportes)
 	pos=0
 	archivo_logico_reportes.seek (0,0)
-    variable=pickle.load(archivo_logico_reportes)
-    while archivo_logico_reportes.tell()<tam 
-    	pos=archivo_logico_reportes.tell()
-    	variable=pickle.load(archivo_logico_reportes)
-    if :archivo_logico_reportes==2
-    	cont=cont+1
-    	return cont
+	variable=pickle.load(archivo_logico_reportes)
+	while archivo_logico_reportes.tell()<tam 
+		pos=archivo_logico_reportes.tell()
+		variable=pickle.load(archivo_logico_reportes)
+		if :archivo_logico_reportes==2
+			cont=cont+1
+	return cont
